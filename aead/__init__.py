9b30bc2dd3a976f69f604f4bb62a8f74fdf87088
Terry Chia <terrycwk1994@gmail.com>
2014-11-30 16:27:40 +0800

    Add initial API design.

---
+++ aead/__init__.py
@@ -0,0 +1,14 @@
+class AEAD(object):
+
+    def __init__(self, key):
+        pass
+
+    @classmethod
+    def generate_key():
+        pass
+
+    def encrypt(self, data, additional_data):
+        pass
+
+    def decrypt(self, data, additional_data):
+        pass
